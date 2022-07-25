b92b9078ba423dfb99c725199d6b8a623a606d5d
“DnyaneshwarNNTX” <dnyaneshwar.wakchaure@netnautix.com>
2022-07-25 20:09:08 +0530

    fixes for freezing issue for collection view

--- CollectionViewDemo/ViewController.swift
+++ CollectionViewDemo/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout,CellDelegate{
-
     var direction: String?
     var indexPathRow: Int?
     var cellSize: CGSize?
@@ -29,20 +28,21 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-            // self.collectionView.reloadData()
+        // self.collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
         if(cellSize != nil){
             if indexArray.contains(obj: indexPath.row){
                 return cellSize!
             }else{
                 let width = (collectionView.frame.width - cellSize!.width - 30.0)/2
                 let height = (collectionView.frame.height-cellSize!.height - 30.0)/2
-
-                return CGSize(width: width < 0 ? 10 : width , height: height < 0 ? 10 : height)
+                return CGSize(width: width < 10 ? 10 : width , height: height < 10 ? 10 : height)
             }
         }
+        
         return CGSize(width: (collectionView.frame.width-50)/3, height: (collectionView.frame.width-50)/3)
     }
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -56,63 +56,52 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         cell.myLabel.text = self.items[indexPath.row]
         cell.backgroundColor = UIColor.cyan
         cell.delegate = self
-        
         return cell
     }
     
     func pinchGestureCallBack(scale: Float, mode: String, cell: UICollectionViewCell) {
-        let indexPath = collectionView.indexPath(for: cell)
-        if(indexPathRow == nil){
-            indexPathRow = indexPath?.row
-        }
-        direction = mode
-
-        if(mode == "H"){
-//            var row = indexPathRow
-//            for index in 0...2 {
-//                if(indexPathRow - 3 <= 0){
-//                    indexArray.append(row)
-//                    row = row+3
-//                }else{
-//                    indexArray.append(row)
-//                    row = row + 3
-//
-//                }
-//
-//            }
-            if(indexPathRow == 0 || indexPathRow == 3||indexPathRow == 6){
-                indexArray = [0,3,6]
-            }else if(indexPathRow == 1 || indexPathRow == 4||indexPathRow == 7){
-                indexArray = [1,4,7]
-            }else if(indexPathRow == 2 || indexPathRow == 5||indexPathRow == 8){
-                indexArray = [2,5,8]
-            }
-            cellSize = CGSize(width:cell.frame.size.width * CGFloat(scale) , height:cell.frame.size.height )
-        }else if(mode == "V"){
-            if(indexPathRow == 0 || indexPathRow == 1||indexPathRow == 2){
-                indexArray = [0,1,2]
-            }else if(indexPathRow == 3 || indexPathRow == 4||indexPathRow == 5){
-                indexArray = [3,4,5]
-            }else if(indexPathRow == 6 || indexPathRow == 7||indexPathRow == 8){
-                indexArray = [6,7,8]
-            }
-            cellSize = CGSize(width:cell.frame.size.width , height:cell.frame.size.height * CGFloat(scale))
-
-        }
-
         DispatchQueue.main.async {
+            let indexPath = self.collectionView.indexPath(for: cell)
+            if(self.indexPathRow == nil){
+                self.indexPathRow = indexPath?.row
+            }
+            self.direction = mode
+            if(mode == "H"){
+                if(self.indexPathRow == 0 || self.indexPathRow == 3||self.indexPathRow == 6){
+                    self.indexArray = [0,3,6]
+                }else if(self.indexPathRow == 1 || self.indexPathRow == 4||self.indexPathRow == 7){
+                    self.indexArray = [1,4,7]
+                }else if(self.indexPathRow == 2 || self.indexPathRow == 5||self.indexPathRow == 8){
+                    self.indexArray = [2,5,8]
+                }
+                let width = ((self.collectionView.frame.width-50)/3) * CGFloat(scale)
+                
+                self.cellSize = CGSize(width:width > self.collectionView.frame.width-100 ? self.collectionView.frame.width-100 : width , height:cell.frame.size.height )
+            }else if(mode == "V"){
+                if(self.indexPathRow == 0 || self.indexPathRow == 1||self.indexPathRow == 2){
+                    self.indexArray = [0,1,2]
+                }else if(self.indexPathRow == 3 || self.indexPathRow == 4||self.indexPathRow == 5){
+                    self.indexArray = [3,4,5]
+                }else if(self.indexPathRow == 6 || self.indexPathRow == 7||self.indexPathRow == 8){
+                    self.indexArray = [6,7,8]
+                }
+                let height = ((self.collectionView.frame.width-50)/3) * CGFloat(scale)
+                self.cellSize = CGSize(width:cell.frame.size.width , height:height > self.collectionView.frame.height-100 ? self.collectionView.frame.height-100 : height)
+                
+            }
             self.collectionView.reloadData()
-        }
 
+        }
+        
     }
     func gestureCallBack(isEnded: Bool) {
         if(isEnded){
-            indexPathRow = nil
+            self.indexPathRow = nil
         }
     }
 }
 extension Array {
-     func contains<T>(obj: T) -> Bool where T: Equatable {
-         return !self.filter({$0 as? T == obj}).isEmpty
-     }
- }
+    func contains<T>(obj: T) -> Bool where T: Equatable {
+        return !self.filter({$0 as? T == obj}).isEmpty
+    }
+}
